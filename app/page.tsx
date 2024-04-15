d59f313ff8ae26edef3f5f38950b9a2806103223
ketanip <ketaniralepatil1@gmail.com>
2024-04-15 23:24:44 +0530

    Initial commit.

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,131 @@
-import Image from "next/image";
+"use client";
+import { useState } from "react";
+
+const winning_moves = [
+  [0,1,2],
+  [3,4,5],
+  [6,7,8],
+  [0,4,8],
+  [2,4,6],
+  [0,3,6],
+  [1,4,7],
+  [2,5,8],
+];
 
 export default function Home() {
+  const [values, setValues] = useState<string[]>([
+    " ",
+    " ",
+    " ",
+    " ",
+    " ",
+    " ",
+    " ",
+    " ",
+    " ",
+  ]);
+
+  const [xOrO, setXOrO] = useState("X");
+  const [lastIndex, setLastIndex] = useState<number>(-1);
+
+  const switchXO = () => {
+    if (xOrO == "X") setXOrO("O");
+    else setXOrO("X");
+  }
+
+  const setVal = (index: number) => {
+    if(values[index] != " ") return;
+    values[index] = xOrO;
+    setLastIndex(index);
+    switchXO();
+    setTimeout(() => checkWinner(), 100)
+  };
+
+  const clearAll = () => {
+    setValues([" ", " ", " ", " ", " ", " ", " ", " ", " "]);
+    setLastIndex(-1);
+    setXOrO("X")
+  };
+
+  const undoMove = () => {
+    if (lastIndex != -1) {
+      values[lastIndex] = " "; 
+      setLastIndex(-1);
+      switchXO();
+    }
+    switchXO();
+  }
+
+  const checkWinner = () => {
+    winning_moves.forEach(item => {
+      
+      if (values[item[0]] === values[item[1]] && values[item[1]] === values[item[2]] && values[item[0]] != " ") {
+        switchXO()
+        alert(xOrO + " has won!!");
+        clearAll();
+      }
+
+    })
+
+    let num_x = 0, num_0 = 0;
+    values.map(item => {
+      if (item == "X" ){num_x++};
+      if(item == "O") {num_0++}
+    });
+    if(num_0 + num_x == 9) {
+      alert(" Match is tie!");
+      clearAll();
+    }
+  }
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+    <div className="max-w-lg sm:px-10  py-8 p-4 flex flex-col gap-4 mx-auto">
+
+        <h2 className="text-center text-4xl my-4 font-bold">The X-O Game!</h2>
+
+      <div className="grid grid-cols-2 text-center border-2  rounded-md">
+        
+        {xOrO == "X" ? (
+          <>
+          <div className="bg-sky-400 text-white font-semibold py-4">Player 1 ( X )</div>
+          <div className="py-4">Player 2 ( 0 )</div>
+          </>
+        ) : (
+          <>
+        <div className="py-4">Player 1 ( X )</div>
+        <div className="bg-teal-400 text-white font-semibold py-4">Player 2 ( 0 )</div>
+          </>
+        )}
+      </div>
+
+      <div className="grid grid-cols-3 gap-2">
+        {values.map((val, index) => (
+          <span
+            key={index}
+            onClick={(e) => setVal(index)}
+            className={
+
+              " p-4 py-8 text-center cursor-pointer rounded bg-gray-100 text-white font-black" 
+              + (val =="X" ? " bg-sky-400  " :" bg-gray-100") 
+              + (val =="O" ? " bg-teal-400" :" bg-gray-100")
+              
+
+              }
           >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
+            {val == " " ? <span className="text-gray-100">Z</span> : <>{val}</>}
+          </span>
+        ))}
       </div>
+      <div className="flex gap-4 ">
+        <button
+          className="bg-red-500 hover:bg-red-600 shadow hover:shadow-lg  px-4 py-2 rounded text-white"
+          onClick={(e) => clearAll()}
+        >
+          Clear All
+        </button>
 
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+        <button disabled={lastIndex == -1} onClick={e => undoMove()} className="disabled:bg-stale-100 disabled:hover:cursor-not-allowed bg-slate-500 hover:bg-slate-600 shadow hover:shadow-lg  px-4 py-2 rounded text-white">Undo</button>
       </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+    </div>
   );
 }
